03f5ecbf0c07e8f0b65154700598a4caccae77ec
Azizur Rahman <Sarzil@azizurs-macbook-pro.local>
2016-02-12 16:46:22 -0500

    Basic Flicks files

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Azizur Rahman on 2/12/16.
+//  Copyright © 2016 Azizur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
