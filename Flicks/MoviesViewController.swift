c6c368c9289f11f1a91949cd43e10a40a312a569
Azizur Rahman <Sarzil@azizurs-macbook-pro.local>
2016-02-13 17:54:50 -0500

    flicks almost there

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -14,14 +14,26 @@ import MBProgressHUD
 
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
 
     @IBOutlet weak var tableView: UITableView!
-    
-    
     var movies: [NSDictionary]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        
+        
+        let alertController = UIAlertController(title: "No Internet Connection", message: "Please connect to a wifi/data connection to view contents", preferredStyle: .Alert)
+
+        
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -56,18 +68,44 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             
                             self.movies = (responseDictionary["results"] as! [NSDictionary])
                             self.tableView.reloadData()
-                            
-                            
-                            
                     }
                 }
-        })
+                else {
+                    let refreshAction = UIAlertAction(title: "Try again", style: .Cancel) { (action) in
+                       self.viewDidLoad()                   }
+                    // add the OK action to the alert controller
+                    alertController.addAction(refreshAction)
+                    
+                    self.presentViewController(alertController, animated: true) {
+                        
+                    }
+                    
+                    
+                    
+                    
+                }
+            }
+        
+        )
         task.resume()
         
         
 
         // Do any additional setup after   loading the view.
     }
+    
+    
+    
+    // additional stuffs
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -89,23 +127,106 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
+        
+        if let posterPath = movie["poster_path"] as? String {
+            
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         
         let imageUrl = NSURL(string: baseUrl + posterPath)
         
+        
+        cell.posterView.setImageWithURL(imageUrl!)
+        }
+        else {
+            cell.posterView.image = nil
+        }
+        
+        
+        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
-        
-        
-        
-        
         print("row \(indexPath.row)")
         
         return cell
     }
+    
+    
+    
+    // refresh function
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        
+        //for alert controller
+        
+        
+        let alertController = UIAlertController(title: "No Internet Connection", message: "Please connect to a wifi/data connection to view contents", preferredStyle: .Alert)
+        
+
+        
+        // ... Create the NSURLRequest (myRequest) ...
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+      
+                
+                let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+                    completionHandler: { (dataOrNil, response, error) in
+                        
+            
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                        print("response: \(responseDictionary)")
+                        self.movies = (responseDictionary["results"] as![NSDictionary])
+                        self.tableView.reloadData()
+                                    
+                                    
+                                    
+                        // Tell the refreshControl to stop spinning
+                        refreshControl.endRefreshing()
+                            }
+                        }
+                        
+                else {
+                    let refreshAction = UIAlertAction(title: "Try again", style: .Cancel) { (action) in
+                        self.viewDidLoad()                   }
+                    
+                    // add the refresh action to the alert controller
+                    alertController.addAction(refreshAction)
+                    
+                    self.presentViewController(alertController, animated: true) {
+                    
+                        refreshControl.endRefreshing()
+                        
+                    }
+                    
+                    
+                    
+                    
+                        }
+                    
+                    }
+                    
+                )
+                task.resume()
+        
+        }
+
+    
+    
 
     /*
     // MARK: - Navigation
