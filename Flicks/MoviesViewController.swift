727995917b97f315b286956e49d3eba2e9b48f11
Azizur Rahman <Sarzil@Azizurs-MacBook-Pro.local>
2016-02-20 19:44:02 -0500

    selection effect of the cell added

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -111,6 +111,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
@@ -135,6 +136,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         print("row \(indexPath.row)")
+        // Setting Cell selected color
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.lightGrayColor()
+        cell.selectedBackgroundView = backgroundView
+        
+        
         
         return cell
     }
